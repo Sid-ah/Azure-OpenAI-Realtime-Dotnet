391f6b3f48ce4e3b3735cdae2154c0133dcda511
ctjoumas <chris.tjoumas@microsoft.com>
2025-06-19 07:08:00 -0400

    Fixing typo in method name

--- realtime-api-dotnet/Models/SessionRequest.cs
+++ realtime-api-dotnet/Models/SessionRequest.cs
@@ -3,5 +3,5 @@ using realtime_api_dotnet.Prompts;
 public class SessionRequest
 {
     public string Voice { get; set; }
-    public string SystemPrompt { get; set; } = CorePrompts.GetSystemmPrompt();
+    public string SystemPrompt { get; set; } = CorePrompts.GetSystemPrompt();
 }
